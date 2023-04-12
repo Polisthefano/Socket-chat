f55a62a84972c0e5fbd484b7c9244e9d1090511c
Sthefano <stouma@slingr.io>
2023-04-12 14:00:05 -0300

    last changes

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,5 +1,5 @@
 let usuario = null;
-let socket = null;
+let socketServer = null;
 let url = window.location.hostname.includes("localhost")
   ? "http://"
   : "https://"; //contiene el host donde me encuentro
@@ -37,7 +37,7 @@ const validarJWT = async () => {
 };
 
 const conectarSocket = async () => {
-  const socketServer = io({
+  socketServer = io({
     extraHeaders: {
       //para mandar info
       token_access: localStorage.getItem("token"),
@@ -52,14 +52,51 @@ const conectarSocket = async () => {
     console.log("sockets offline");
   });
 
-  socketServer.on("recibir-mensajes", () => {});
+  socketServer.on("recibir-mensajes", dibujarMensajes);
 
-  socketServer.on("usuarios-activos", (payload) => {
+  socketServer.on("usuarios-activos", dibujarUsuarios); //llamamos directamente al callback
+
+  socketServer.on("mensaje-privado", (payload) => {
     console.log(payload);
   });
-
-  socketServer.on("mensaje-privado", () => {});
 };
+
+const dibujarUsuarios = (usuarios = []) => {
+  let usersHtml = "";
+  usuarios.forEach((user) => {
+    usersHtml += `<li>
+    <p>
+    <h5 class="text-success">${user.nombre}</h5>
+    <span class="fs-6 text-muted">${user.uid}</span>
+    </p>
+    </li>`;
+  });
+  ulUsuarios.innerHTML = usersHtml;
+};
+const dibujarMensajes = (mensajes = []) => {
+  let mensajesHtml = "";
+  mensajes.forEach(({ mensaje, nombre }) => {
+    mensajesHtml += `<li>
+    <p>
+    <span class="text-primary">${nombre}:</span>
+    <span>${mensaje}</span>
+    </p>
+    </li>`;
+  });
+  ulMensajes.innerHTML = mensajesHtml;
+};
+
+txtMsg.addEventListener("keyup", ({ keyCode }) => {
+  const msg = txtMsg.value;
+  const uid = txtUid.value;
+  if (keyCode !== 13) return;
+  else {
+    if (msg.trim().length != 0) {
+      socketServer.emit("enviar-mensaje", { msg, uid });
+      txtMsg.value = "";
+    }
+  }
+});
 
 const main = async () => {
   await validarJWT();
--- sockets/controller.socket.js
+++ sockets/controller.socket.js
@@ -5,12 +5,31 @@ const chatMensajes = new ChatMensajes();
 const socketController = async (socket, io) => {
   const usuario = await comprobarJWT(socket.handshake.headers.token_access);
   if (!usuario) return socket.disconnect(); //si no existe el usuario o esta inactivo o eliminado lo desconectamos
-  console.log("Conectado:", usuario.nombre);
+
+  //agregar usuario
   chatMensajes.conectarUsuario(usuario);
   io.emit("usuarios-activos", chatMensajes.usuariosToArr); //como estoy usando io que es todo el servidor, no tengo que emitir con emit al que me envio la peticion y broadcast a todos los otros
+  socket.emit("recibir-mensajes", chatMensajes.last10);
+
+  //socket join crea una sala privada, porque ya por default se crea una sola sala global donde estan todos los eventos
+  //donde el primer parametro es el identificador de la sala
+  socket.join(usuario.id);
+
   socket.on("disconnect", () => {
     chatMensajes.desconectarUsuario(usuario.id);
     io.emit("usuarios-activos", chatMensajes.usuariosToArr);
+  });
+
+  socket.on("enviar-mensaje", ({ uid, msg }) => {
+    if (uid) {
+      //mensaje privado
+      socket
+        .to(uid)
+        .emit("mensaje-privado", { from: usuario.nombre, mensaje: msg }); //el to permite enviar por nombre a una sala en particular, la definimos en la linea 15
+    } else {
+      chatMensajes.enviarMensaje(usuario.id, usuario.nombre, msg);
+      io.emit("recibir-mensajes", chatMensajes.last10);
+    }
   });
 };
 module.exports = { socketController };
